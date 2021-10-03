52712dd5eea81af929afbf2df33e5f91e0876604
Ivan Boban <ivan.boban911@gmail.com>
2021-10-03 17:35:27 +0200

    Removed stuff from inital section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import "./App.css";
-import sofry from "./assets/sofry.jpg";
+import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <div className="card">
-        <img src={sofry} alt="sofa with rest as a pack of fries" />
-        <p className="desc">
-          <div className="art-title"> So Fry</div>
-          <div className="art-date">2021.</div>
-        </p>
-      </div>
+      <Card />
     </div>
   );
 }
